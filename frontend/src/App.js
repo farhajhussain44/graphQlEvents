f0cfb12989e20cc3ba25ce129b9b349aabf6dbe5
farhajhussain44 <farhajhussain29@gmail.com>
2020-06-09 00:17:14 +0530

    nav section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,23 @@ import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import AuthPage from './pages/auth';
 import EventPage from './pages/events';
 import BookingPage from './pages/bookings';
+import MainNavigation from './components/Navigation/mainNavigation';
+import './App.css'
 const App = () => {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Redirect from='/' to='/authpage' exact />
-          <Route path='/authpage' component={AuthPage} />
-          <Route path='/events' component={EventPage} />
-          <Route path='/bookings' component={BookingPage} />
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <React.Fragment>
+        <MainNavigation />
+        <main className="main-content">
+          <Switch>
+            <Redirect from="/" to="/authpage" exact />
+            <Route path="/authpage" component={AuthPage} />
+            <Route path="/events" component={EventPage} />
+            <Route path="/bookings" component={BookingPage} />
+          </Switch>
+        </main>
+      </React.Fragment>
+    </BrowserRouter>
   );
 }
 export default App;
